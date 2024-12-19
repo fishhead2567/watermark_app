80e41a6d7255f41f86de5f2cf1b0a1584d498e0e
Andrew Best <andrew.best@tri.global>
2024-12-19 00:30:00 -0600

    Update pyinstaller config

--- watermark_app/make_dist.sh
+++ watermark_app/make_dist.sh
@@ -1,2 +1,11 @@
-pyinstaller -F watermark_gui.py
-pyinstaller -F watermark_cli.py
+#!/bin/bash
+
+# Figure out where the code lives
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# make a directory into which pyinstaller should install files
+
+mkdir -p build
+
+poetry run pyinstaller -F $SCRIPT_DIR/watermark_gui.py --specpath build
+poetry run pyinstaller -F $SCRIPT_DIR/watermark_cli.py --specpath build
